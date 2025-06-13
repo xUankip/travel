826ecebdcc122c586106a1dbd4fd2841f0cc0b8d
xU <142771851+xUankip@users.noreply.github.com>
2025-06-13 19:32:12 +0700

    init provider

--- travel/src/main/java/com/travel/HelloServlet.java
+++ travel/src/main/java/com/travel/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "aaaaaaa" + "</h1>");
         out.println("</body></html>");
     }
 
